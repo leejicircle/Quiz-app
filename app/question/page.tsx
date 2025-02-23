43ddaa397f360ee6deaa17739693669d22fdca7f
wldnj <wldnjs1277@naver.com>
2025-02-23 10:31:58 +0900

    ...

--- app/question/page.tsx
+++ app/question/page.tsx
@@ -1,8 +1,24 @@
+import Question from '@/components/Question'
+import { getQuestions } from '@/utils/functions'
 import React from 'react'
 
 const QuestionPage = () => {
+
+  const questions = getQuestions()
+
   return (
-    <div>QuestionPage</div>
+    <div
+      className={'flex flex-col space-y-4 md:space-y-8 p-4 lg:p-8'}
+    >
+      {questions.map((question) => (
+        <Question
+          withId={false}
+          checkEnabled={true}
+          key={question.id}
+          question={question}
+        />
+      ))}
+    </div>
   )
 }
 
