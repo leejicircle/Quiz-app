df66ff040079d3229e271dcbbf1d9e176e7edff1
wldnj <wldnjs1277@naver.com>
2025-03-04 18:50:21 +0900

    SelectState 컴포넌트 추가

--- components/State.tsx
+++ components/State.tsx
@@ -8,20 +8,20 @@ import Question from './Question'
 
 interface Props {
   states: IState[]
-  
+
 }
-const State = ({states}: Props) => {
-const [state, setState] = useState(0)
+const State = ({ states }: Props) => {
+  const [state, setState] = useState(0)
 
   return (
     <div>
-      {/*<SelectState states={states}></SelectState>*/}
+      <SelectState states={states} state={(value) => setState(value)} />
       <div className={'flex flex-col space-y-4 md:space-y-8 pt-4'}>
         {states[state].questions.map((question) => (
           <Question
             withId={true}
             checkEnabled={true}
-            key={question.id+"_"+state}
+            key={question.id + "_" + state}
             question={question}
           />
         ))}
