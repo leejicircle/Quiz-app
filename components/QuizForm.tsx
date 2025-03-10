221d55a5402da22dd675180ca71e01d1d1136a9f
wldnj <wldnjs1277@naver.com>
2025-03-10 16:34:23 +0900

    quiz 컴포넌트 수정 및 결과화면 추가

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -30,6 +30,17 @@ const QuizForm = () => {
     setAnswers([...answers, currentAnswer])
     setAnswered(false)
   }
+  const passed = () => answers.filter(a => a).length >= 2
+  
+  const newStart = () => {
+    setQuizStarted(false)
+    setAnswered(false)
+    setcurrentAnswer(false)
+    setState(1)
+    setCurrentQuestion(0)
+    setQuestions([])
+    setQuizEnded(false)
+  }
 
   return (
     <div
@@ -74,14 +85,39 @@ const QuizForm = () => {
             {answered && (
               <button
                 className='my-4 px-8 py-2 text-white float-right rounded-lg shadow bg-blue-600 hover:bg-blue-500'
-                onClick={() => { currentQuestion > questions.length - 1
-                  ? QuizEnd()
-                  : nextQuestion()
-                 }}>
-                {currentQuestion > questions.length - 1 ? "완료" : "다음"}
+                onClick={() => {
+                  currentQuestion >= questions.length - 1
+                    ? QuizEnd()
+                    : nextQuestion()
+                }}>
+                {currentQuestion >= questions.length - 1 ? "완료" : "다음"}
               </button>
             )}
           </div>
+        </div>
+      )}
+      {QuizEnded && (
+        <div className={'bg-slate-50 p-8 rounded-xl shadow'}>
+          <div className={'text-4xl font-semibold mb-8'}>결과</div>
+          <div>
+            <span className={'font-bold'}>{questions.length}</span> 점 중 {" "}
+            <span
+              className={`font-bold ${passed() ? "text-green-600" : "text-red-600"}`}
+            >
+              {answers.filter((a) => a).length}
+            </span>{" "}
+            점을 획득했습니다.
+          </div>
+          <div className={'my-4'}>
+            {passed() ? "축하합니다." : "탈락"}
+          </div>
+          <button
+            className={`my-4 px-8 py-2 text-white float-right rounded-lg shadow
+                    bg-blue-600 hover:bg-blue-500`}
+            onClick={newStart}
+          >
+            새로운 연습 테스트 시작
+          </button>
         </div>
       )}
     </div>
